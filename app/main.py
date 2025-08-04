180e238b36b9aff8a4de5e64303b1a15c2da0029
vinudasenith <ha.vinudas@gmail.com>
2025-08-04 16:51:14 +0530

    add method to return scorea👍

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI,File,UploadFile,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.parser import parse_resume,extract_text_from_pdf
-from app.ranker import score_resume
+from app.ranker import (score_resume,get_score_grade,calculate_ats_compatibility,generate_smart_recommendations)
 
 app=FastAPI(title="Resume Parser Microservice (No LLM)")
 
@@ -27,11 +27,31 @@ async def parse_resume_endpoint(file: UploadFile = File(...)):
 
     parsed_data = parse_resume(text)
 
-    score = score_resume(parsed_data, job_description="Software Engineer")  
+    score_data = score_resume(parsed_data, job_description="Software Engineer")  
+
+
+    if score_data is None:
+        return{
+            "parsed_data":parsed_data,
+            "resume_score":None,
+            "score_grade":None,
+            "ats_compatibility":0,
+            "smart_tips":"Resume scoring failed",
+            # "raw_text":text
+        }
+    
+    resume_score=score_data.get("score",0)
+    score_grade=get_score_grade(resume_score)
+    ats_compatibility=score_data.get("ats_compatibility",0)
+    smart_tips=score_data.get("smart_tips",[])
+
 
     return {
         "parsed_data": parsed_data,
-        "resume_score": score,
-        "raw_text": text
+        "resume_score": resume_score,
+        "score_grade": score_grade,
+        "ats_compatibility": ats_compatibility,
+        "smart_tips": smart_tips,
+        # "raw_text": text
     }
 
